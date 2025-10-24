c1a8962c71052fc2bd38553f569dc3c43bfd3dc3
Abdulla Azizulla <aazizulla@vendasta.com>
2025-10-24 12:20:13 +0530

    Fix wildcard route for Express v5

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendBuildPath));
 
   // serve index.html for any other route (so React Router works)
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(frontendBuildPath, 'index.html'));
   });
 } else {
